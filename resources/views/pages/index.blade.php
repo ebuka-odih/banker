ebf1315da9c3df39d58b3842d4747b35451c2001
Gnosis <emmagab38@gmail.com>
2023-06-07 12:27:54 +0100

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,358 @@
+@extends('pages.layouts.app')
+@section('content')
+
+<!-- ##### Hero Area Start ##### -->
+<div class="hero-area">
+    <div class="hero-slideshow owl-carousel">
+
+        <!-- Single Slide -->
+        <div class="single-slide bg-img">
+            <!-- Background Image-->
+            <div class="slide-bg-img bg-img bg-overlay" style="background-image: url(img/bg-img/6.gif);"></div>
+            <!-- Welcome Text -->
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center">
+                    <div class="col-12 col-lg-9">
+                        <div class="welcome-text text-center">
+                            <h6 data-animation="fadeInUp" data-delay="100ms">Our Online banking is</h6>
+                            <h2 data-animation="fadeInUp" data-delay="300ms">Securely <span>Encrypted</span></h2>
+                            <p data-animation="fadeInUp" data-delay="500ms">With over 35 years of banking experience ,we’ll ensure you always get the best!.</p>
+                            <a href="office/index.html" class="btn credit-btn mt-50" data-animation="fadeInUp" data-delay="700ms">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Slide Duration Indicator -->
+            <div class="slide-du-indicator"></div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="single-slide bg-img">
+            <!-- Background Image-->
+            <div class="slide-bg-img bg-img bg-overlay" style="background-image: url(img/bg-img/5.jpg);"></div>
+            <!-- Welcome Text -->
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center">
+                    <div class="col-12 col-lg-9">
+                        <div class="welcome-text text-center">
+                            <h6 data-animation="fadeInDown" data-delay="100ms">We can help manage</h6>
+                            <h2 data-animation="fadeInDown" data-delay="300ms">your <span>investments<span> </h2>
+                            <p data-animation="fadeInDown" data-delay="500ms">Our team has many years of experience and they are all active and available to serve you better!.</p>
+                            <a href="office/index.html" class="btn credit-btn mt-50" data-animation="fadeInDown" data-delay="700ms">Discover</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Slide Duration Indicator -->
+            <div class="slide-du-indicator"></div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="single-slide bg-img">
+            <!-- Background Image-->
+            <div class="slide-bg-img bg-img bg-overlay" style="background-image: url(img/bg-img/1.jpg);"></div>
+            <!-- Welcome Text -->
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center">
+                    <div class="col-12 col-lg-9">
+                        <div class="welcome-text text-center">
+                            <h6 data-animation="fadeInUp" data-delay="100ms">Feel free to </h6>
+                            <h2 data-animation="fadeInUp" data-delay="300ms">Contact <span>Our Support</span></h2>
+                            <p data-animation="fadeInUp" data-delay="500ms">We are available to help you with any of your banking needs and we make every effort to ensure contacting us is as easy as possible.Always there to help.</p>
+                            <a href="#" class="btn credit-btn mt-50" data-animation="fadeInUp" data-delay="700ms">Discover</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Slide Duration Indicator -->
+            <div class="slide-du-indicator"></div>
+        </div>
+
+        <!-- Single Slide -->
+
+
+    </div>
+</div>
+<!-- ##### Hero Area End ##### -->
+
+<!-- ##### Features Area Start ###### -->
+<section class="features-area section-padding-100-0">
+    <div class="container">
+        <div class="row align-items-end">
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single-features-area mb-100 wow fadeInUp" data-wow-delay="100ms">
+                    <!-- Section Heading -->
+                    <div class="section-heading">
+                        <div class="line"></div>
+                        <p>Take look at our</p>
+                        <h2>Internet Banking</h2>
+                    </div>
+                    <h6>Handle all your corporate banking functions from one place and amaze yourself at how much easier it can be to run your world.</h6>
+                    <a href="office/index.html" class="btn credit-btn mt-50">Login Now</a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single-features-area mb-100 wow fadeInUp" data-wow-delay="300ms">
+                    <img src="img/bg-img/2.jpg" alt="">
+                    <h5>We take care of you</h5>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single-features-area mb-100 wow fadeInUp" data-wow-delay="500ms">
+                    <img src="img/bg-img/3.jpg" alt="">
+                    <h5>No documents needed</h5>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single-features-area mb-100 wow fadeInUp" data-wow-delay="700ms">
+                    <img src="img/bg-img/4.jpg" alt="">
+                    <h5>Fast &amp; easy transactions</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ##### Features Area End ###### -->
+
+<!-- ##### Call To Action Start ###### -->
+<section class="cta-area d-flex flex-wrap">
+    <!-- Cta Thumbnail -->
+    <div class="cta-thumbnail bg-img jarallax" style="background-image: url(img/bg-img/5.jpg);"></div>
+
+    <!-- Cta Content -->
+    <div class="cta-content">
+        <!-- Section Heading -->
+        <div class="section-heading white">
+            <div class="line"></div>
+            <p>Both within and beyond</p>
+            <h2>Helping small businesses like yours</h2>
+        </div>
+        <h6>There are numerous reasons for starting a business, including pursuing a passion, wanting to set your own hours and wanting to make more money. we are here to help you actualize this goal</h6>
+        <div class="d-flex flex-wrap mt-50">
+            <!-- Single Skills Area -->
+            <div class="single-skils-area mb-70 mr-5">
+                <div id="circle" class="circle" data-value="0.90">
+                    <div class="skills-text">
+                        <span>90%</span>
+                    </div>
+                </div>
+                <p>Energy</p>
+            </div>
+
+            <!-- Single Skills Area -->
+            <div class="single-skils-area mb-70 mr-5">
+                <div id="circle2" class="circle" data-value="0.75">
+                    <div class="skills-text">
+                        <span>75%</span>
+                    </div>
+                </div>
+                <p>power</p>
+            </div>
+
+            <!-- Single Skills Area -->
+            <div class="single-skils-area mb-70">
+                <div id="circle3" class="circle" data-value="0.97">
+                    <div class="skills-text">
+                        <span>97%</span>
+                    </div>
+                </div>
+                <p>resource</p>
+            </div>
+        </div>
+        <!-- <a href="#" class="btn credit-btn box-shadow btn-2">Read More</a> -->
+    </div>
+</section>
+<!-- ##### Call To Action End ###### -->
+
+<!-- ##### Call To Action Start ###### -->
+<section class="cta-2-area wow fadeInUp" data-wow-delay="100ms">
+    <div class="col-12">
+        <div class="elements-title mb-30">
+            <div class="line"></div>
+            <h2 style="color: white;">Milestone</h2>
+        </div>
+    </div>
+
+    <div class="col-12">
+        <div class="credit-cool-facts-area" style="color: antiquewhite;">
+            <div class="row">
+
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <!-- Single Cool Facts -->
+                    <div class="single-cool-fact d-flex align-items-center mb-100">
+                        <div class="scf-icon mr-15">
+                            <i class="icon-piggy-bank"></i>
+                        </div>
+                        <div class="scf-text">
+                            <h2 style="color: white;"><span class="counter">8710</span></h2>
+                            <p style="color: white;">Clients</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <!-- Single Cool Facts -->
+                    <div class="single-cool-fact d-flex align-items-center mb-100 text-center">
+                        <div class="scf-icon mr-15">
+                            <i class="icon-coin"></i>
+                        </div>
+                        <div class="scf-text">
+                            <h2 style="color: white;"><span class="counter">35</span></h2>
+                            <p style="color: white;">Creditors</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-lg-4 text-center mx-auto">
+                    <!-- Single Cool Facts -->
+                    <div class="single-cool-fact d-flex align-items-center mb-100">
+                        <div class="scf-icon mr-15">
+                            <i class="icon-diamond"></i>
+                        </div>
+                        <div class="scf-text">
+                            <h2 style="color: white;"><span class="counter">12</span></h2>
+                            <p style="color: white;">awards</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+                <div class="cta-content d-flex flex-wrap align-items-center justify-content-between">
+                    <div class="cta-text">
+                        <h4>Are you in need for a load? Get in touch with us.</h4>
+                    </div>
+                    <div class="cta-btn">
+                        <a href="#" class="btn credit-btn box-shadow">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</section>
+<!-- ##### Call To Action End ###### -->
+
+<!-- ##### Services Area Start ###### -->
+<section class="services-area section-padding-100-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Section Heading -->
+                <div class="section-heading text-center mb-100 wow fadeInUp" data-wow-delay="100ms">
+                    <div class="line"></div>
+                    <p>Take look at our</p>
+                    <h2>Our services</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <!-- Single Service Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single-service-area d-flex mb-100 wow fadeInUp" data-wow-delay="200ms">
+                    <div class="icon">
+                        <i class="icon-profits"></i>
+                    </div>
+                    <div class="text">
+                        <h5>secure Online Banking</h5>
+                        <p>we provide clients with the best online banking experience all from the comfort of your home.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Service Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single-service-area d-flex mb-100 wow fadeInUp" data-wow-delay="300ms">
+                    <div class="icon">
+                        <i class="icon-money-1"></i>
+                    </div>
+                    <div class="text">
+                        <h5>Easy and fast answer</h5>
+                        <p>Our team of experienced support agents are ever ready to assist with all your possible needs.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Service Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single-service-area d-flex mb-100 wow fadeInUp" data-wow-delay="400ms">
+                    <div class="icon">
+                        <i class="icon-coin"></i>
+                    </div>
+                    <div class="text">
+                        <h5>Cash Back</h5>
+                        <p>Earn up to 15% cash back at locations you choose when you use your eligible loyds Bank debit card After your purchase, cash back is automatically credited to the account of your choice..</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Service Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single-service-area d-flex mb-100 wow fadeInUp" data-wow-delay="500ms">
+                    <div class="icon">
+                        <i class="icon-smartphone-1"></i>
+                    </div>
+                    <div class="text">
+                        <h5>Secure financial services</h5>
+                        <p>We employ high tech encryption techniques to ensure smooth and safe transactions across.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Service Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single-service-area d-flex mb-100 wow fadeInUp" data-wow-delay="600ms">
+                    <div class="icon">
+                        <i class="icon-diamond"></i>
+                    </div>
+                    <div class="text">
+                        <h5>Good investments</h5>
+                        <p> we are here to provide customers with the best investment packages to grow your money faster.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Service Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single-service-area d-flex mb-100 wow fadeInUp" data-wow-delay="700ms">
+                    <div class="icon">
+                        <i class="icon-piggy-bank"></i>
+                    </div>
+                    <div class="text">
+                        <h5>Accumulation goals</h5>
+                        <p> As your account balance grows, your interest rate has the potential to increase too.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ##### Services Area End ###### -->
+
+<!-- ##### Newsletter Area Start ###### -->
+<section class="newsletter-area section-padding-100 bg-img jarallax" style="background-image: url(img/bg-img/6.jpg);">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-lg-8">
+                <div class="nl-content text-center">
+                    <h2>Subscribe to our newsletter</h2>
+                    <form action="#" method="post">
+                        <input type="email" name="nl-email" id="nlemail" placeholder="Your e-mail">
+                        <button type="submit">Subscribe</button>
+                    </form>
+                    <!-- <p>Curabitur elit turpis, maximus quis ullamcorper sed, maximus eu neque. Cras ultrices erat nec auctor blandit.</p> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ##### Newsletter Area End ###### -->
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'pages.index')->name('index');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/credit-card', 'pages.creditcard')->name('creditcard');
+Route::view('/mortages', 'pages.mortages')->name('mortages');
+Route::view('/investment', 'pages.investment')->name('investment');
+Route::view('/retirement', 'pages.retirement')->name('retirement');
+Route::view('/services', 'pages.services')->name('services');
+Route::view('/faq', 'pages.faq')->name('faq');
+Route::view('/contact', 'pages.contact')->name('contact');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
